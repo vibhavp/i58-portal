7eb3c8c95fc2609611a664759a2efa3f36a39d12
Vibhav Pant <vibhavp@gmail.com>
2016-08-09 00:15:09 +0530

    IsLoggedIn is publi

--- controllers/api/admin/login.go
+++ controllers/api/admin/login.go
@@ -7,7 +7,7 @@ import (
 	"github.com/vibhavp/i58-portal/controllers/api"
 )
 
-func isLoggedIn(r *http.Request) bool {
+func IsLoggedIn(r *http.Request) bool {
 	if cookie, err := r.Cookie("login"); err == nil {
 		return cookie.Value ==
 			api.LoginHash(config.Config.Username, config.Config.Password)
