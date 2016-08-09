2510ae3c6261ba6ee49deb65fb320ae75774c7df
Vibhav Pant <vibhavp@gmail.com>
2016-08-09 23:12:57 +0530

    Change colors

--- js/graph.js
+++ js/graph.js
@@ -52,7 +52,7 @@ window.addEventListener('load', function () {
 		datasets: [{
 		    label: label,
 		    data: dataset,
-		    backgroundColor: 'rgba(0, 91, 168, 1)',
+		    backgroundColor: 'rgba(48, 63, 159, 1)',
 		}]
 	    },
 	};
@@ -79,17 +79,18 @@ window.addEventListener('load', function () {
 		    data: dataset,
 		    fill: false,
 		    lineTension: 0.1,
-		    backgroundColor: "rgba(0, 91, 168, 1)",
+		    backgroundColor: "rgba(255, 193, 7, 0.93)",
 		    borderCapStyle: 'butt',
 		    borderDash: [],
 		    borderDashOffset: 0.0,
 		    borderJoinStyle: 'miter',
-		    pointBorderColor: "rgba(75,192,192,1)",
+		    borderColor: "rgba(48, 63, 159, 0.93)",
+		    pointBorderColor: "rgba(255, 193, 7, 0.93)",
 		    pointBackgroundColor: "#fff",
 		    pointBorderWidth: 1,
 		    pointHoverRadius: 5,
-		    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-		    pointHoverBorderColor: "rgba(220,220,220,1)",
+		    pointHoverBackgroundColor: "rgba(255, 193, 7, 0.93)",
+		    pointHoverBorderColor: "rgba(255, 193, 7, 0.93)",
 		    pointHoverBorderWidth: 2,
 		    pointRadius: 1,
 		    pointHitRadius: 10,
@@ -124,7 +125,7 @@ window.addEventListener('load', function () {
 
     var ctx = document.getElementById("myChart");
     var param = "?class=" + getParam("class");
-    var url = "/api/stats/get_stats";
+    var url = "/api/stats/get_stats?class=" + getParam("class");
 
     if (getParam("class") == null) {
 	if (getParam("playerid") != null) {
