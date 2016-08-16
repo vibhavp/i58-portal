748a7f7e1c8d642de20d4d7a55f9822175363de3
Vibhav Pant <vibhavp@gmail.com>
2016-08-16 15:22:07 +0530

    add DB_DRIVER and DB_URL ev vars

--- config/config.go
+++ config/config.go
@@ -7,12 +7,19 @@ import (
 )
 
 var Config = struct {
-	Username string `envconfig:"USERNAME" required:"true"`
-	Password string `envconfig:"PASSWORD" required:"true"`
+	Username string `envconfig:"USERNAME" default:"i58"`
+	Password string `envconfig:"PASSWORD" default:"i58"`
 	Address  string `envconfig:"ADDRESS" default:":8080"`
+	DBDriver string `envconfig:"DB_DRIVER" default:"sqlite3"`
+
+	DBURL string `envconfig:"DB_URL" default:"logs.db"`
 }{}
 
 func init() {
-	envconfig.MustProcess("PORTAL", &Config)
+	log.SetFlags(log.Lshortfile)
+	err := envconfig.Process("PORTAL", &Config)
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Printf("username: %s, password %s", Config.Username, Config.Password)
 }
