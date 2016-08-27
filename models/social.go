c5c6d63b5e06384e97313210cecad1b60019caa4
Vibhav Pant <vibhavp@gmail.com>
2016-08-27 14:53:20 +0530

    reverse tweet lis

--- models/social.go
+++ models/social.go
@@ -20,5 +20,9 @@ func AddTweet(url string) error {
 func GetAllTweets() []Tweet {
 	var tweets []Tweet
 	db.DB.Order("id desc").Limit(10).Find(&tweets)
+	for i := 0; i < len(tweets)/2; i++ {
+		j := len(tweets) - i - 1
+		tweets[i], tweets[j] = tweets[j], tweets[i]
+	}
 	return tweets
 }
